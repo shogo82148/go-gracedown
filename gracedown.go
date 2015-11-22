8aaad4a5e27f8156878b55887922a3633fb14fb9
Ichinose Shogo <shogo82148@gmail.com>
2015-11-23 01:01:25 +0900

    init commit

---
+++ gracedown.go
@@ -0,0 +1,32 @@
+package gracedown
+
+import (
+	"net"
+	"net/http"
+)
+
+type Server struct {
+	Server *http.Server
+}
+
+func NewWithServer(s *http.Server) *GracefulServer {
+	return &Server{
+		Server: s,
+	}
+}
+
+func (srv *Server) ListenAndServe() error {
+	addr := srv.Addr
+	if addr == "" {
+		addr = ":http"
+	}
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	return srv.Serve(ln)
+}
+
+func (srv *Server) Serve(l net.Listener) error {
+	return srv.Server.Serve(l)
+}
